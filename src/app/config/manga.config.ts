359f2e9b8d47052408c8f083c6c6bbdc6484e0b5
Francisco <fran.7id@gmail.com>
2026-03-17 16:12:49 -0300

    Add cover image for Solo Leveling manga

--- src/app/config/manga.config.ts
+++ src/app/config/manga.config.ts
@@ -4,7 +4,7 @@ export const MANGA_LIST: MangaConfig[] = [
   {
     id: 'comic-solo_leveling_1691379040764',
     title: 'Solo Leveling',
-    coverImage: 'assets/sololeveling.jpeg',
+    coverImage: 'sololeveling.jpeg',
     totalChapters: 200,
     apiBaseUrl:
       'https://manhwawebbackend-production.up.railway.app/chapters/see/comic-solo_leveling_1691379040764-',
